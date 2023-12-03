5363aa8adcde3a82c216a9718e1dbb7fa778cf12
Patrick <patrickrodriguez2002@outlook.com>
2023-12-03 19:35:01 -0300

    .

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,57 +7,57 @@ import { ReactComponent as Link } from "./assets/linkedin.svg";
 import { ReactComponent as Wpp } from "./assets/wpp.svg";
 
 const Footer = () => {
-    const socialMedias = [
-        {
-            id: 0,
-            icon: <Git />,
-            link: "https://github.com/ptrktodev",
-            descr: "GitHub",
-        },
-        {
-            id: 1,
-            icon: <Insta />,
-            link: "https://github.com/ptrktodev",
-            descr: "Instagram",
-        },
-        {
-            id: 2,
-            icon: <Link />,
-            link: "https://github.com/ptrktodev",
-            descr: "Linkedin",
-        },
-        {
-            id: 3,
-            icon: <Wpp />,
-            link: "https://github.com/ptrktodev",
-            descr: "Whatsapp",
-        },
-    ];
+  const socialMedias = [
+    {
+      id: 0,
+      icon: <Git />,
+      link: "https://github.com/ptrktodev",
+      descr: "GitHub",
+    },
+    {
+      id: 1,
+      icon: <Insta />,
+      link: "https://www.instagram.com/patrickrdz3103/",
+      descr: "Instagram",
+    },
+    {
+      id: 2,
+      icon: <Link />,
+      link: "https://www.linkedin.com/in/ptrktodev/",
+      descr: "Linkedin",
+    },
+    {
+      id: 3,
+      icon: <Wpp />,
+      link: "https://wa.me/5551980702533",
+      descr: "Whatsapp",
+    },
+  ];
 
-    return (
-        <footer>
-            <div className="containerFooter">
-                <div className="FooterCard">
-                    {" "}
-                    <h1 className="logo">
-                        TechWeb <br />
-                        Sync{" "}
-                    </h1>
-                    <p> ©Alguns direitos reservados.</p>
-                </div>
-                <div className="FooterCard">
-                    {socialMedias.map((i) => (
-                        <div key={i.id} className="socialItems">
-                            <span>{i.icon}</span>
-                            <a href={i.link} target="_blank">
-                                {i.descr}
-                            </a>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <footer>
+      <div className="containerFooter">
+        <div className="FooterCard">
+          {" "}
+          <h1 className="logo">
+            TechWeb <br />
+            Sync{" "}
+          </h1>
+          <p> ©Alguns direitos reservados.</p>
+        </div>
+        <div className="FooterCard">
+          {socialMedias.map((i) => (
+            <div key={i.id} className="socialItems">
+              <span>{i.icon}</span>
+              <a href={i.link} target="_blank">
+                {i.descr}
+              </a>
             </div>
-        </footer>
-    );
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/Components/Projetos/Projetos.jsx
+++ src/Components/Projetos/Projetos.jsx
@@ -6,77 +6,81 @@ import Tasks from "./assets/TaskApp.png";
 import AppMobileParty from "./assets/mobileApp.png";
 
 const Projetos = () => {
-    const infosProjetos = [
-        {
-            id: 0,
-            title: "CineMagic",
-            descr: "Neste Universo do CineMagic você pode obter informações sobre filmes e séries.",
-            link: "https://cinemagic.bohr.io/",
-            img: CineMagic,
-            colorBorder: "#ff1f1f",
-        },
-        {
-            id: 1,
-            title: "PicSure",
-            descr: "Em uma pequena galeria, você tem acesso a milhões de imagens.",
-            link: "https://picsure.bohr.io/",
-            img: Picsure,
-            colorBorder: "#3b8cff",
-        },
-        {
-            id: 2,
-            title: "Little Links",
-            descr: "Mini Projeto que contém link úteis para entrar em contato comigo.",
-            link: "https://littlelinks.bohr.io/",
-            img: Linksme,
-            colorBorder: "#31ff62",
-        },
-        {
-            id: 3,
-            title: "To-do.Tasks",
-            descr: "Anote suas tarefas, edite ou apague. Ganhe mais agilidade no dia a dia.",
-            link: "https://github.com/ptrktodev/taskApp",
-            img: Tasks,
-            colorBorder: "#f6d000",
-        },
-        {
-            id: 4,
-            title: "Party Royal",
-            descr: "Aplicativo mobile para marcar as suas festas mais divertidas.",
-            link: "https://github.com/ptrktodev/PartyRoyal-AppMobile",
-            img: AppMobileParty,
-            colorBorder: "#9827ff",
-        },
-    ];
+  const infosProjetos = [
+    {
+      id: 0,
+      title: "CineMagic",
+      descr:
+        "Neste Universo do CineMagic você pode obter informações sobre filmes e séries.",
+      link: "https://cinemagic.bohr.io/",
+      img: CineMagic,
+      colorBorder: "#ff1f1f",
+    },
+    {
+      id: 1,
+      title: "PicSure",
+      descr: "Em uma pequena galeria, você tem acesso a milhões de imagens.",
+      link: "https://picsure.bohr.io/",
+      img: Picsure,
+      colorBorder: "#3b8cff",
+    },
+    {
+      id: 2,
+      title: "Little Links",
+      descr:
+        "Mini Projeto que contém link úteis para entrar em contato comigo.",
+      link: "https://littlelinks.bohr.io/",
+      img: Linksme,
+      colorBorder: "#31ff62",
+    },
+    {
+      id: 3,
+      title: "To-do.Tasks",
+      descr:
+        "Anote suas tarefas, edite ou apague. Ganhe mais agilidade no dia a dia.",
+      link: "https://github.com/ptrktodev/taskApp",
+      img: Tasks,
+      colorBorder: "#f6d000",
+    },
+    {
+      id: 4,
+      title: "Party Royal",
+      descr:
+        "Aplicativo mobile para marcar as suas festas mais divertidas e convidar seus amigos.",
+      link: "https://github.com/ptrktodev/PartyRoyal-AppMobile",
+      img: AppMobileParty,
+      colorBorder: "#9827ff",
+    },
+  ];
 
-    return (
-        <section id="projetos">
-            <div className="containerAuto">
-                <h1>
-                    Projetos <span className="grad">.</span>
-                </h1>
-                <div className="CardsProjetos">
-                    {infosProjetos.map((i) => (
-                        <div
-                            key={i.id}
-                            className="Cards"
-                            style={{ border: `1px solid ${i.colorBorder}` }}
-                        >
-                            <h2>{i.title}</h2>
-                            <p>{i.descr}</p>
-                            <a
-                                href={i.link}
-                                target="_blank"
-                                style={{ color: ` ${i.colorBorder}` }}
-                            >
-                                Visualizar Projeto
-                            </a>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <section id="projetos">
+      <div className="containerAuto">
+        <h1>
+          Projetos <span className="grad">.</span>
+        </h1>
+        <div className="CardsProjetos">
+          {infosProjetos.map((i) => (
+            <div
+              key={i.id}
+              className="Cards"
+              style={{ border: `1px solid ${i.colorBorder}` }}
+            >
+              <h2>{i.title}</h2>
+              <p>{i.descr}</p>
+              <a
+                href={i.link}
+                target="_blank"
+                style={{ color: ` ${i.colorBorder}` }}
+              >
+                Visualizar Projeto
+              </a>
             </div>
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projetos;
