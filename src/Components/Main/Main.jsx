c2845f5e1afb66be6da427dc6e4e5cef4bcacfa6
Patrick <patrickrodriguez2002@outlook.com>
2023-12-03 20:09:06 -0300

    .

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -8,44 +8,42 @@ import { TextPlugin } from "gsap/TextPlugin";
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
 const Main = () => {
-    React.useLayoutEffect(() => {
-        gsap.to(".main", {
-            x: 0,
-            opacity: 1,
-            duration: 1.2,
-        });
-        return () => {
-            gsap.killTweensOf(".main");
-        };
-    }, []);
+  React.useLayoutEffect(() => {
+    gsap.to(".main", {
+      x: 0,
+      opacity: 1,
+      duration: 1.2,
+    });
+    return () => {
+      gsap.killTweensOf(".main");
+    };
+  }, []);
 
-    React.useLayoutEffect(() => {
-        gsap.to(".interfaceDigital", {
-            y: 0,
-            opacity: 1,
-            duration: 5,
-        });
-        return () => {
-            gsap.killTweensOf(".interfaceDigital");
-        };
-    }, []);
+  React.useLayoutEffect(() => {
+    gsap.to(".interfaceDigital", {
+      y: 0,
+      opacity: 1,
+      duration: 5,
+    });
+    return () => {
+      gsap.killTweensOf(".interfaceDigital");
+    };
+  }, []);
 
-    return (
-        <header>
-            <div className="container main">
-                <h1>
-                    Desenvolvendo{" "}
-                    <span className="interfaceDigital">
-                        interfaces digitais
-                    </span>{" "}
-                    <br /> de forma moderna e com <br />
-                    alta qualidade.
-                </h1>
-                <Machine />
-                <span className="barra-colorfull"></span>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div className="container main">
+        <h1>
+          Desenvolvendo{" "}
+          <span className="interfaceDigital">interfaces digitais</span> <br />{" "}
+          de forma moderna e com <br />
+          alta qualidade.
+        </h1>
+        {/*<Machine />*/}
+        <span className="barra-colorfull"></span>
+      </div>
+    </header>
+  );
 };
 
 export default Main;
